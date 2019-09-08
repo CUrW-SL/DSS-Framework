796214add7a0b6d234a7892fe9cd7ecab0b6a866
hasitha <hasithadkr7@gmail.com>
2019-09-08 12:57:22 +0530

    rule engine db schema updated

--- db_scripts/schema.sql
+++ db_scripts/schema.sql
@@ -26,7 +26,6 @@ CREATE TABLE IF NOT EXISTS `hechms_rules`
     `no_observed_continue`        TINYINT(1) DEFAULT NULL,
     `rainfall_data_from`          varchar(100) DEFAULT NULL,
     `ignore_previous_run`         TINYINT(1)  DEFAULT NULL,
-    `check_gfs_data_availability` TINYINT(1)  DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = latin1;
@@ -43,9 +42,10 @@ CREATE TABLE IF NOT EXISTS `flo2d_rules`
     `init_run`                    TINYINT(1)   DEFAULT NULL,
     `no_forecast_continue`        TINYINT(1)   DEFAULT NULL,
     `no_observed_continue`        TINYINT(1)   DEFAULT NULL,
-    `rainfall_data_from`          varchar(100) DEFAULT NULL,
+    `rain_cell_data_from`          varchar(100) DEFAULT NULL,
+    `inflow_data_from`          varchar(100) DEFAULT NULL,
+    `outflow_data_from`          varchar(100) DEFAULT NULL,
     `ignore_previous_run`         TINYINT(1)   DEFAULT NULL,
-    `check_gfs_data_availability` TINYINT(1)   DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = latin1;
