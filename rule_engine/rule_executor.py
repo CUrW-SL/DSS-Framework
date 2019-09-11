05f0be187f2b5c335a3841de0ef88da6425f7e57
hasitha <hasithadkr7@gmail.com>
2019-09-11 16:12:33 +0530

    remove json rule executing

--- rule_engine/rule_executor.py
+++ rule_engine/rule_executor.py
@@ -1,54 +1,3 @@
-import json
-import glob
-
-
-class RuleStore:
-    def __init__(self, rule_dir_path):
-        self.rule_dir_path = rule_dir_path
-        self.all_rule_data = self.load_all_rules()
-
-    def load_rule(self, rule_file_path):
-        with open(rule_file_path) as f:
-            data = json.load(f)
-            return data
-
-    def load_rule_set(self, rule_type='all'):
-        rule_data = {}
-        if rule_type == 'all':
-            rule_file_list = glob.glob("{}/*.{}".format(self.rule_dir_path, 'json'))
-        else:
-            rule_file_list = glob.glob("{}/{}*.{}".format(self.rule_dir_path, rule_type, 'json'))
-        if rule_file_list:
-            for rule_file in rule_file_list:
-                rule_data = self.load_rule(rule_file)
-                rule_data[rule_data['rule_id']] = rule_data
-        else:
-            print('No defined rules.')
-        return rule_data
-
-    def load_all_rules(self):
-        all_rule_data = {}
-        rule_file_list = glob.glob("{}/*.{}".format(self.rule_dir_path, 'json'))
-        if rule_file_list:
-            for rule_file in rule_file_list:
-                rule_data = self.load_rule(rule_file)
-                all_rule_data[rule_data['rule_id']] = rule_data
-        else:
-            print('No defined rules.')
-        return all_rule_data
-
-    def view_all_rules(self):
-        for rule_id in self.all_rule_data:
-            print(self.all_rule_data[rule_id])
-
-
-def execute_exec_rule(rule_store, rule_id):
-    rule_data = rule_store.all_rule_data[rule_id]
-    print('rule_data : ', rule_data)
-    if rule_data['rule_type'] == 'exec' and rule_data['enable'] == 'true':
-        print('')
-    else:
-        print('Rule {} is disabled.'.format(rule_id))
 
 
 def execute_wrf_model_rules():
@@ -63,19 +12,11 @@ def execute_flo2d_model_rules():
     print('')
 
 
-def execute_accuracy_rule():
+def execute_accuracy_rules():
     print()
 
 
-def execute_alert_rule():
+def execute_variable_rules():
     print()
 
-
-def execute_alert_rule():
-    print()
-
-
-rule_store = RuleStore('/home/hasitha/PycharmProjects/DSS-Framework/rule_engine/rule_scripts')
-print('rule_store : ', rule_store)
-execute_exec_rule(rule_store, 'wrf_rule1')
 
