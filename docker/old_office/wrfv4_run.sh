2cd2cf0d279578cb36d133413f9bb75cd3af6350
hasitha <hasithadkr7@gmail.com>
2019-09-24 21:33:00 +0530

    input flag change

--- docker/old_office/wrfv4_run.sh
+++ docker/old_office/wrfv4_run.sh
@@ -5,11 +5,11 @@ echo "#### Reading running args..."
 HOME_DIR='/mnt/disks/data/wrf_run/wrf1'
 
 
-while getopts ":h:d:c:v:r:m:" option; do
+while getopts ":h:d:c:s:r:m:" option; do
   case "${option}" in
   d) EXEC_DATE=$OPTARG ;; # 2019-09-24
   c) CHECK_GFS=$OPTARG ;; # true
-  v) VERSION=$OPTARG ;; # 4.0
+  s) VERSION=$OPTARG ;; # 4.0
   r) RUN=$OPTARG ;; # 0 or 1
   h) HOUR=$OPTARG ;; # 00 or 06 or 12 or 18
   m) MODEL=$OPTARG ;; # A or C or E or SE
@@ -26,12 +26,6 @@ echo "CHECK_GFS : $CHECK_GFS"
 echo "VERSION : $VERSION"
 echo "RUN : $RUN"
 echo "MODEL : $MODEL"
-
-OUTPUT_DIR='${HOME_DIR}/${VERSION}/d${RUN}/${HOUR}/${EXEC_DATE}/${MODEL}'
-GFS_DIR='${HOME_DIR}/${VERSION}/d${RUN}/${HOUR}/${EXEC_DATE}/gfs'
-ARCHIVE_DIR='${HOME_DIR}/${VERSION}/d${RUN}/${HOUR}/${EXEC_DATE}/${MODEL}/archive'
-
-echo "OUTPUT_DIR : $OUTPUT_DIR"
 
 echo "#### Running WRF procedures..."
 cd /home/Build_WRF/code
