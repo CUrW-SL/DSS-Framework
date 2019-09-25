a4403a55d928407315afe6e1f1567adfadfe2a0c
hasitha <hasithadkr7@gmail.com>
2019-09-25 06:08:56 +0530

    getting gfs url

--- docker/old_office/wrfv4_run.sh
+++ docker/old_office/wrfv4_run.sh
@@ -5,7 +5,7 @@ echo "#### Reading running args..."
 HOME_DIR='/mnt/disks/data/wrf_run/wrf1'
 
 
-while getopts ":h:d:c:s:r:m:" option; do
+while getopts ":h:d:c:s:r:m:u:" option; do
   case "${option}" in
   d) EXEC_DATE=$OPTARG ;; # 2019-09-24
   c) CHECK_GFS=$OPTARG ;; # true
@@ -13,6 +13,7 @@ while getopts ":h:d:c:s:r:m:" option; do
   r) RUN=$OPTARG ;; # 0 or 1
   h) HOUR=$OPTARG ;; # 00 or 06 or 12 or 18
   m) MODEL=$OPTARG ;; # A or C or E or SE
+  U) GFS_URL=$OPTARG ;; # A or C or E or SE
   esac
 done
 
@@ -26,6 +27,7 @@ echo "CHECK_GFS : $CHECK_GFS"
 echo "VERSION : $VERSION"
 echo "RUN : $RUN"
 echo "MODEL : $MODEL"
+echo "GFS_URL : $GFS_URL"
 
 echo "#### Running WRF procedures..."
 cd /home/Build_WRF/code
@@ -37,5 +39,6 @@ python3 wrfv4_run.py \
                     $( check_empty "$VERSION" version ) \
                     $( check_empty "$RUN" run ) \
                     $( check_empty "$HOUR" hour ) \
+                    $( check_empty "$GFS_URL" gfs_url ) \
                     $( check_empty "$MODEL" model )
 echo "####WRF procedures completed"
