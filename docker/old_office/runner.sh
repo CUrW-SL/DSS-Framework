be147235dfe2c8a01dfc236ee2a1fe02203e0f08
hasitha <hasithadkr7@gmail.com>
2019-09-25 20:47:44 +0530

    exec date formatting

--- docker/old_office/runner.sh
+++ docker/old_office/runner.sh
@@ -6,9 +6,10 @@ HOME_DIR="/mnt/disks/data/wrf_run/wrf1"
 GFS_URL="ftp://ftpprd.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.{RUN_DATE}/{DATA_HOUR}"
 GFS_DOWNLOAD_FILE_TEMPLATE="gfs.t{DATA_HOUR}z.pgrb2.0p50.f0{INDEX}"
 
-while getopts ":h:d:c:s:r:m:" option; do
+while getopts ":h:d:t:c:s:r:m:" option; do
   case "${option}" in
-  d) EXEC_DATE=$OPTARG ;; # 2019-09-24_04:00
+  d) EXEC_DATE=$OPTARG ;; # 2019-09-24
+  t) EXEC_TIME=$OPTARG ;; # 04:00
   c) CHECK_GFS=$OPTARG ;; # true
   s) VERSION=$OPTARG ;; # 4.0
   r) RUN=$OPTARG ;; # 0 or 1
@@ -18,6 +19,7 @@ while getopts ":h:d:c:s:r:m:" option; do
 done
 
 echo "EXEC_DATE : $EXEC_DATE"
+echo "EXEC_TIME : $EXEC_TIME"
 echo "HOME_DIR : $HOME_DIR"
 echo "HOUR : $HOUR"
 echo "CHECK_GFS : $CHECK_GFS"
@@ -28,6 +30,7 @@ echo "MODEL : $MODEL"
 OUTPUT_DIR="${HOME_DIR}/${VERSION}/d${RUN}/${HOUR}/${EXEC_DATE}/${MODEL}"
 GFS_DIR="${HOME_DIR}/${VERSION}/d${RUN}/${HOUR}/${EXEC_DATE}/gfs"
 ARCHIVE_DIR="${HOME_DIR}/${VERSION}/d${RUN}/${HOUR}/${EXEC_DATE}/${MODEL}/archive"
+START_DATE="${EXEC_DATE}_${EXEC_TIME}"
 
 mkdir -p ${OUTPUT_DIR}
 echo "OUTPUT_DIR : $OUTPUT_DIR"
@@ -36,4 +39,4 @@ echo "GFS_DIR : $GFS_DIR"
 mkdir -p ${ARCHIVE_DIR}
 echo "ARCHIVE_DIR : $ARCHIVE_DIR"
 
-docker run -i --rm --privileged -v /mnt/disks/data/samba/wrf-static-data/geog:/home/Build_WRF/geog curw-wrfv4:ubuntu1604  /home/Build_WRF/code/wrfv4_run.sh -d ${EXEC_DATE} -h ${HOUR} -c ${CHECK_GFS}  -s ${VERSION} -r ${RUN} -m ${MODEL} -u ${GFS_URL} -v ${OUTPUT_DIR}:/home/Build_WRF/nfs  -v ${GFS_DIR}:/home/Build_WRF/gfs -v ${ARCHIVE_DIR}:/home/Build_WRF/archive
+docker run -i --rm --privileged -v /mnt/disks/data/samba/wrf-static-data/geog:/home/Build_WRF/geog curw-wrfv4:ubuntu1604  /home/Build_WRF/code/wrfv4_run.sh -d ${START_DATE} -h ${HOUR} -c ${CHECK_GFS}  -s ${VERSION} -r ${RUN} -m ${MODEL} -u ${GFS_URL} -v ${OUTPUT_DIR}:/home/Build_WRF/nfs  -v ${GFS_DIR}:/home/Build_WRF/gfs -v ${ARCHIVE_DIR}:/home/Build_WRF/archive
