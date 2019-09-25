96142096b3d874105e5b26789892fa5e9b469990
hasitha <hasithadkr7@gmail.com>
2019-09-25 17:29:02 +0530

    update args

--- docker/old_office/code/wrfv4_run.sh
+++ docker/old_office/code/wrfv4_run.sh
@@ -2,8 +2,6 @@
 
 echo "#### Reading running args..."
 
-HOME_DIR='/mnt/disks/data/wrf_run/wrf1'
-
 
 while getopts ":h:d:c:s:r:m:u:" option; do
   case "${option}" in
@@ -21,7 +19,6 @@ check_empty() {
   [ -z "$1" ] && echo "" || echo "-$2=$1"
 }
 echo "EXEC_DATE : $EXEC_DATE"
-echo "HOME_DIR : $HOME_DIR"
 echo "HOUR : $HOUR"
 echo "CHECK_GFS : $CHECK_GFS"
 echo "VERSION : $VERSION"
@@ -33,7 +30,6 @@ echo "#### Running WRF procedures..."
 cd /home/Build_WRF/code
 echo "Inside $(pwd)"
 python3 wrfv4_run.py \
-                    $( check_empty "$HOME_DIR" home_dir ) \
                     $( check_empty "$EXEC_DATE" exec_date ) \
                     $( check_empty "$CHECK_GFS" check_gfs ) \
                     $( check_empty "$VERSION" version ) \
