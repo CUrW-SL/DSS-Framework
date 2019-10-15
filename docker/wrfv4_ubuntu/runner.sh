8e8b3df0d5a8c6b23fa5def5c5ac5edaa3cd64c3
hasitha <hasithadkr7@gmail.com>
2019-10-15 13:54:56 +0530

    add version to shell script

--- docker/wrfv4_ubuntu/runner.sh
+++ docker/wrfv4_ubuntu/runner.sh
@@ -4,31 +4,33 @@ echo "#### Running WRF procedures..."
 
 mkdir -p /mnt/disks/workspace1/wrf-data/gfs
 
-while getopts ":r:m:h:" option; do
+while getopts ":r:m:v:h:" option; do
   case "${option}" in
   r) WRF_RUN=$OPTARG ;; # 1/0
   m) MODEL=$OPTARG ;; # 1/0
-  h) v4=$OPTARG ;; # 00/06/12/18
+  v) VERSION=$OPTARG ;; # 1/0
+  h) GFS_HOUR=$OPTARG ;; # 00/06/12/18
   esac
 done
 
 echo "WRF_RUN : $WRF_RUN"
 echo "GFS_HOUR : $GFS_HOUR"
 echo "MODEL : $MODEL"
+echo "VERSION : $VERSION"
 
 if [ ${WRF_RUN} == 0 ] || [ ${WRF_RUN} == "0" ]; then
     echo ""
     tmp_date=`date '+%Y-%m-%d' --date="1 days ago"`
     gfs_date="${tmp_date}_${GFS_HOUR}:00"
     exec_date=`date '+%Y-%m-%d'`
-    wrf_id="wrfv4_${WRF_RUN}_${GFS_HOUR}_${exec_date}"
+    wrf_id="dwrf_${VERSION}_${WRF_RUN}_${GFS_HOUR}_${exec_date}"
 fi
 
 if [ ${WRF_RUN} == 1 ] || [ ${WRF_RUN} == "1" ]; then
     tmp_date=`date '+%Y-%m-%d'`
     gfs_date="${tmp_date}_${GFS_HOUR}:00"
     exec_date=`date '+%Y-%m-%d'`
-    wrf_id="wrfv4_${WRF_RUN}_${GFS_HOUR}_${exec_date}"
+    wrf_id="dwrf_${VERSION}_${WRF_RUN}_${GFS_HOUR}_${exec_date}"
 fi
 
 echo "gfs_date ${gfs_date}"
