fd200415151c788e417956ca9eab6fb6dc825f0a
hasitha <hasithadkr7@gmail.com>
2019-10-18 13:15:01 +0530

    set user inputs in bash script and call python script from it

--- docker/wrfv4_ubuntu/rfielder.sh
+++ docker/wrfv4_ubuntu/rfielder.sh
@@ -2,6 +2,45 @@
 
 echo `date`
 
+while getopts ":r:m:v:h:" option; do
+  case "${option}" in
+  r) WRF_RUN=$OPTARG ;; # 1/0
+  m) MODEL=$OPTARG ;; # 1/0
+  v) VERSION=$OPTARG ;; # 1/0
+  h) GFS_HOUR=$OPTARG ;; # 00/06/12/18
+  esac
+done
+
+BASE_DIR='/mnt/disks/wrf_nfs/dwrf'
+
+echo "WRF_RUN : $WRF_RUN"
+echo "GFS_HOUR : $GFS_HOUR"
+echo "MODEL : $MODEL"
+echo "VERSION : $VERSION"
+
+if [ ${WRF_RUN} == 0 ] || [ ${WRF_RUN} == "0" ]; then
+    echo ""
+    tmp_date=`date '+%Y-%m-%d' --date="1 days ago"`
+    gfs_date="${tmp_date}_${GFS_HOUR}:00"
+    exec_date=`date '+%Y-%m-%d'`
+    run_id="dwrf_${VERSION}_${WRF_RUN}_${GFS_HOUR}_${exec_date}_${MODEL}"
+fi
+
+if [ ${WRF_RUN} == 1 ] || [ ${WRF_RUN} == "1" ]; then
+    tmp_date=`date '+%Y-%m-%d'`
+    gfs_date="${tmp_date}_${GFS_HOUR}:00"
+    exec_date=`date '+%Y-%m-%d'`
+    run_id="dwrf_${VERSION}_${WRF_RUN}_${GFS_HOUR}_${exec_date}_${MODEL}"
+fi
+
+echo "gfs_date : ${gfs_date}"
+echo "exec_date : ${exec_date}"
+echo "run_id : ${run_id}"
+
+check_empty() {
+  [ -z "$1" ] && echo "" || echo "-$2=$1"
+}
+
 echo "Changing into /home/uwcc-admin/wrf_docker"
 cd /home/uwcc-admin/wrf_docker
 echo "Inside `pwd`"
@@ -19,19 +58,19 @@ echo "Activating venv python3 virtual environment."
 source venv/bin/activate
 
 # Install dependencies using pip.
-if [ ! -f "curw_sim_utils.log" ]
+if [ ! -f "wrfv4_rfied.log" ]
 then
     echo "Installing Pandas"
     pip install pandas
     echo "Installing netCDF4"
     pip install netCDF4
-    touch docker_rfield.log
+    touch wrfv4_rfied.log
 fi
-
 
 # Run rfield generating scripts
 echo "Running update_obs_rainfall_flo2d_250.py"
-python rain/flo2d_250/fcst/update_fcst_rainfall_flo2d_250.py >> rain/flo2d_250/fcst/curw_sim_fcst_flo2d_250.log 2>&1
+python code/wrfv4_rfied.py $( check_empty "${run_id}" wrf_id )  \
+                            $( check_empty "${BASE_DIR}" base_dir ) >> docker_rfield.log 2>&1
 
 # Deactivating virtual environment
 echo "Deactivating virtual environment"
