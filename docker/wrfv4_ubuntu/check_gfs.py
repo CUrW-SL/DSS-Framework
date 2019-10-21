785516af043db2adff039fc51cbde84708862d39
hasitha <hasithadkr7@gmail.com>
2019-10-21 11:39:21 +0530

    add gfs ftp server connecting module

---
+++ docker/wrfv4_ubuntu/check_gfs.py
@@ -0,0 +1,24 @@
+from ftplib import FTP
+
+
+def get_file(ftp, filename):
+    try:
+        ftp.retrbinary('RETR ' + filename, open(filename, 'wb').write)
+    except:
+        print('Error')
+
+
+def connect_gfs_ftp_server():
+    ftp = FTP('ftp.ncep.noaa.gov')
+    ftp.login()
+    print(ftp.pwd())
+    ftp.cwd('/pub/data/nccf/com/gfs/prod/gfs.20191020/18/')
+    print(ftp.retrlines('LIST'))
+
+
+if __name__ == '__main__':
+    GFS_HOME = '/pub/data/nccf/com/gfs/prod/gfs.20191020/18/'
+
+    GFS_HOME = '/pub/data/nccf/com/gfs/prod/gfs.{}/{}/'
+    connect_gfs_ftp_server()
+
