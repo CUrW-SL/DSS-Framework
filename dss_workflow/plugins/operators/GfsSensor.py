512572b9a2cccee1eefd826de030bbb1726ef5e4
hasitha <hasithadkr7@gmail.com>
2019-10-23 11:21:10 +0530

    add gfs sensor data availability checking

--- dss_workflow/plugins/operators/GfsSensor.py
+++ dss_workflow/plugins/operators/GfsSensor.py
@@ -1,8 +1,49 @@
 from airflow.exceptions import AirflowSensorTimeout
 from airflow.operators.sensors import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
-from datetime import datetime, timedelta
-import requests
+from datetime import datetime
+from ftplib import FTP
+
+
+def check_gfs_data(gfs_hour,
+                   last_gfs_file_format='gfs.t{}z.pgrb2.0p50.f075',
+                   check_dt=None,
+                   gfs_server='ftp.ncep.noaa.gov',
+                   gfs_home='/pub/data/nccf/com/gfs/prod'):
+    """
+    :param check_dt: string 2019-10-21 11:43:00
+    :return:
+    """
+    gfs_data_downloadable = False
+    print('check_gfs_data|check_dt: ', check_dt)
+    print('check_gfs_data|gfs_server: ', gfs_server)
+    print('check_gfs_data|gfs_home: ', gfs_home)
+    print('check_gfs_data|last_gfs_file_format: ', last_gfs_file_format)
+    if check_dt is not None:
+        gfs_dt = datetime.strptime(check_dt, '%Y-%m-%d %H:%M:%S')
+    else:
+        gfs_dt = datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
+    print('check_gfs_data|gfs_dt: ', gfs_dt)
+    ftp = FTP(gfs_server)
+    ftp.login()
+    gfs_date = gfs_dt.strftime('%Y%m%d')
+    ftp_cwd = '{}/gfs.{}/{}/'.format(gfs_home, gfs_date, gfs_hour)
+    print('check_gfs_data|ftp_cwd: ', ftp_cwd)
+    try:
+        change_dir = ftp.cwd(ftp_cwd)
+        print('change_dir : ', change_dir)
+        last_gfs_file = last_gfs_file_format.format(gfs_hour)
+        print('last_gfs_file : ', last_gfs_file)
+        file_list = ftp.nlst()
+        if len(file_list) > 0:
+            for gfs_file in file_list:
+                if gfs_file == last_gfs_file:
+                    gfs_data_downloadable = True
+    except Exception as e:
+        print('change_dir|Exception : ', str(e))
+    finally:
+        print('check_gfs_data|gfs_data_downloadable : ', gfs_data_downloadable)
+        return gfs_data_downloadable
 
 
 class GFSsensor(BaseSensorOperator):
@@ -16,15 +57,12 @@ class GFSsensor(BaseSensorOperator):
     def poke(self, context):
         try:
             print('-----------------------------------------------------------------------')
-            wrf_date = self.wrf_date
-            gfs_hour = self.gfs_hour
-            result = requests.get(url=url)
-            print('check_file_status|result : ', result.json())
-            rest_data = result.json()
+            condition = check_gfs_data(self.gfs_hour, check_dt=self.wrf_date)
+            print('GFSsensor|condition : ', condition)
             print('-----------------------------------------------------------------------')
-            return rest_data['completed']
+            return condition
         except AirflowSensorTimeout as to:
             self._do_skip_downstream_tasks(context)
-            raise AirflowSensorTimeout('Snap. Time is OUT.')
+            raise AirflowSensorTimeout('GFSsensor. Time is OUT.')
 
 
