9e806b9e414081b2b589ee7e55aa13eb42a395f6
hasitha <hasithadkr7@gmail.com>
2019-11-02 14:18:46 +0530

    child bash script run error fix

--- docker/cloud/testing.sh
+++ docker/cloud/testing.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "starting test script"
-./home/hasitha/PycharmProjects/DSS-Framework/docker/cloud/test_child.sh
+/home/hasitha/PycharmProjects/DSS-Framework/docker/cloud/test_child.sh
 return_code=$?
 echo "return_code : ${return_code}"
 echo "end of test script"
