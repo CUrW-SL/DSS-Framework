ead5f3ce73379bd7591147644d482b8b3e14e4f3
hasitha <hasithadkr7@gmail.com>
2019-11-06 12:19:42 +0530

    add prints

--- dss_workflow/dags/workflow_controller.py
+++ dss_workflow/dags/workflow_controller.py
@@ -31,7 +31,6 @@ def init_workflow_routine(**context):
     routine = adapter.get_next_workflow_routines(run_date)
     print('init_workflow_routine|routine : ', routine)
     if routine is None:
-        set_running_state(adapter, routine['id'])
         return {'id': 0, 'dss1': 0, 'dss2': 0, 'dss3': 0, 'schedule': ''}
     else:
         set_running_state(adapter, routine['id'])
