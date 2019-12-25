b079b90a4c21c56144ab98c62565676ce120d67d
hasitha <hasithadkr7@gmail.com>
2019-12-25 15:41:04 +0530

    add new column as rule_details to rule tables to store extra info

--- db_util/scripts/schema.sql
+++ db_util/scripts/schema.sql
@@ -15,35 +15,37 @@ CREATE TABLE IF NOT EXISTS `workflow_routines`
 
 CREATE TABLE IF NOT EXISTS `wrf_rules`
 (
-      `id`                          int(11) NOT NULL AUTO_INCREMENT,
-      `name`                        varchar(45) DEFAULT NULL,
-      `status`                      int(11) DEFAULT NULL COMMENT '0 - disable, 1 - enable, 2 - running, 3 - completed',
-      `target_model`                varchar(45) DEFAULT NULL,
-      `version`                     varchar(45) DEFAULT NULL,
-      `run`                         varchar(45) DEFAULT NULL,
-      `hour`                        varchar(45) DEFAULT NULL,
-      `ignore_previous_run`         TINYINT(1) DEFAULT NULL,
-      `check_gfs_data_availability` TINYINT(1) DEFAULT NULL,
-      `triggered_by`                int(11) DEFAULT NULL,
-      PRIMARY KEY (`id`),
-      PRIMARY KEY (`name`)
+    `id`                          int(11) NOT NULL AUTO_INCREMENT,
+    `name`                        varchar(45) DEFAULT NULL,
+    `status`                      int(11)     DEFAULT NULL COMMENT '0 - disable, 1 - enable, 2 - running, 3 - completed',
+    `target_model`                varchar(45) DEFAULT NULL,
+    `version`                     varchar(45) DEFAULT NULL,
+    `run`                         varchar(45) DEFAULT NULL,
+    `hour`                        varchar(45) DEFAULT NULL,
+    `ignore_previous_run`         TINYINT(1)  DEFAULT NULL,
+    `check_gfs_data_availability` TINYINT(1)  DEFAULT NULL,
+    `rule_details`                varchar(45) DEFAULT NULL,
+    `triggered_by`                int(11)     DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    PRIMARY KEY (`name`)
 ) ENGINE=InnoDB
   DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `hechms_rules`
 (
-    `id`                          int(11) NOT NULL AUTO_INCREMENT,
-    `name`                        varchar(45) DEFAULT NULL,
-    `status`                      int(11)  DEFAULT NULL COMMENT '0 - disable, 1 - enable, 2 - running, 3 - completed',
-    `target_model`                varchar(45) DEFAULT NULL,
-    `forecast_days`               int(11) DEFAULT NULL,
-    `observed_days`               int(11) DEFAULT NULL,
-    `init_run`                    TINYINT(1) DEFAULT NULL,
-    `no_forecast_continue`        TINYINT(1) DEFAULT NULL,
-    `no_observed_continue`        TINYINT(1) DEFAULT NULL,
-    `rainfall_data_from`          int(11) DEFAULT NULL,
-    `ignore_previous_run`         TINYINT(1)  DEFAULT NULL,
-    `triggered_by`                int(11) DEFAULT NULL,
+    `id`                   int(11) NOT NULL AUTO_INCREMENT,
+    `name`                 varchar(45) DEFAULT NULL,
+    `status`               int(11)     DEFAULT NULL COMMENT '0 - disable, 1 - enable, 2 - running, 3 - completed',
+    `target_model`         varchar(45) DEFAULT NULL,
+    `forecast_days`        int(11)     DEFAULT NULL,
+    `observed_days`        int(11)     DEFAULT NULL,
+    `init_run`             TINYINT(1)  DEFAULT NULL,
+    `no_forecast_continue` TINYINT(1)  DEFAULT NULL,
+    `no_observed_continue` TINYINT(1)  DEFAULT NULL,
+    `rainfall_data_from`   int(11)     DEFAULT NULL,
+    `ignore_previous_run`  TINYINT(1)  DEFAULT NULL,
+    `rule_details`         varchar(45) DEFAULT NULL,
+    `triggered_by`         int(11)     DEFAULT NULL,
     PRIMARY KEY (`id`),
     PRIMARY KEY (`name`)
 ) ENGINE = InnoDB
@@ -51,19 +53,20 @@ CREATE TABLE IF NOT EXISTS `hechms_rules`
 
 CREATE TABLE IF NOT EXISTS `flo2d_rules`
 (
-    `id`                          int(11) NOT NULL AUTO_INCREMENT,
-    `name`                        varchar(45)  DEFAULT NULL,
-    `status`                      int(11) DEFAULT NULL COMMENT '0 - disable, 1 - enable, 2 - running, 3 - completed',
-    `target_model`                varchar(45) DEFAULT NULL,
-    `forecast_days`               int(11) DEFAULT NULL,
-    `observed_days`               int(11) DEFAULT NULL,
-    `no_forecast_continue`        TINYINT(1) DEFAULT NULL,
-    `no_observed_continue`        TINYINT(1) DEFAULT NULL,
-    `raincell_data_from`         int(11) DEFAULT NULL,
-    `inflow_data_from`            int(11) DEFAULT NULL,
-    `outflow_data_from`           int(11) DEFAULT NULL,
-    `ignore_previous_run`         TINYINT(1) DEFAULT NULL,
-    `triggered_by`                int(11) DEFAULT NULL,
+    `id`                   int(11) NOT NULL AUTO_INCREMENT,
+    `name`                 varchar(45) DEFAULT NULL,
+    `status`               int(11)     DEFAULT NULL COMMENT '0 - disable, 1 - enable, 2 - running, 3 - completed',
+    `target_model`         varchar(45) DEFAULT NULL,
+    `forecast_days`        int(11)     DEFAULT NULL,
+    `observed_days`        int(11)     DEFAULT NULL,
+    `no_forecast_continue` TINYINT(1)  DEFAULT NULL,
+    `no_observed_continue` TINYINT(1)  DEFAULT NULL,
+    `raincell_data_from`   int(11)     DEFAULT NULL,
+    `inflow_data_from`     int(11)     DEFAULT NULL,
+    `outflow_data_from`    int(11)     DEFAULT NULL,
+    `ignore_previous_run`  TINYINT(1)  DEFAULT NULL,
+    `rule_details`         varchar(45) DEFAULT NULL,
+    `triggered_by`         int(11)     DEFAULT NULL,
     PRIMARY KEY (`id`),
     PRIMARY KEY (`name`)
 ) ENGINE = InnoDB
