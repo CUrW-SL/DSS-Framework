45b5119deafe303cbc3b50a338baf7b98556f495
hasitha <hasithadkr7@gmail.com>
2019-12-25 08:29:00 +0530

    new wrf E model dag

--- dss_workflow/dags/wrf/wrf_4.1.2_E.py
+++ dss_workflow/dags/wrf/wrf_4.1.2_E.py
@@ -15,7 +15,7 @@ from dss_db import RuleEngineAdapter
 sys.path.insert(0, '/home/uwcc-admin/git/DSS-Framework/accuracy_unit/wrf')
 from wrf_accuracy import calculate_wrf_rule_accuracy
 
-prod_dag_name = 'wrf_4.1.2_C_dag'
+prod_dag_name = 'wrf_4.1.2_E_dag'
 dag_pool = 'wrf_pool'
 
 default_args = {
@@ -28,7 +28,7 @@ default_args = {
 # ./runner.sh -r 0 -m E -v 4.0 -h 18 -d 2019-10-24
 # ./rfielder.sh -r 0 -m E -v 4.0 -h 18
 
-run_wrf4_C_cmd = 'echo "run_wrf_C_cmd" ;sleep $[($RANDOM % 1000) + 1]s'
+run_wrf4_E_cmd = 'echo "run_wrf_E_cmd" ;sleep $[($RANDOM % 1000) + 1]s'
 rfield_gen_cmd = 'echo "rfield_gen_cmd" ;sleep $[($RANDOM % 100) + 1]s'
 data_push_cmd = 'echo "data_push_cmd" ;sleep $[($RANDOM % 10) + 1]s'
 
@@ -46,7 +46,7 @@ def update_workflow_status(status, rule_id):
 
 
 def get_rule_id(context):
-    rule_info = context['task_instance'].xcom_pull(task_ids='init_wrfv4C')['rule_info']
+    rule_info = context['task_instance'].xcom_pull(task_ids='init_wrfv4E')['rule_info']
     if rule_info:
         rule_id = rule_info['id']
         print('get_rule_id|rule_id : ', rule_id)
@@ -73,16 +73,16 @@ def set_complete_status(**context):
 
 def run_this_func(dag_run, **kwargs):
     print('run_this_func|dag_run : ', dag_run)
-    wrf_rule = {'model': 'C', 'version': '4.1.2', 'rule_info': dag_run.conf}
+    wrf_rule = {'model': 'E', 'version': '4.1.2', 'rule_info': dag_run.conf}
     print('run_this_func|wrf_rule : ', wrf_rule)
     return wrf_rule
 
 
 def check_accuracy(**context):
     print('check_accuracy|context : ', context)
-    rule_info = context['task_instance'].xcom_pull(task_ids='init_wrfv4C')['rule_info']
+    rule_info = context['task_instance'].xcom_pull(task_ids='init_wrfv4E')['rule_info']
     print('check_accuracy|rule_info : ', rule_info)
-    wrf_rule = {'model': 'C', 'version': '4.1.2', 'rule_info': rule_info}
+    wrf_rule = {'model': 'E', 'version': '4.1.2', 'rule_info': rule_info}
     print('check_accuracy|wrf_rule : ', wrf_rule)
     exec_date = context["execution_date"].to_datetime_string()
     print('check_accuracy|exec_date : ', wrf_rule)
@@ -91,62 +91,62 @@ def check_accuracy(**context):
 
 
 with DAG(dag_id=prod_dag_name, default_args=default_args, schedule_interval=None,
-         description='Run WRF v4 C DAG', catchup=False) as dag:
-    init_wrfv4_C = PythonOperator(
-        task_id='init_wrfv4C',
+         description='Run WRF v4 E DAG', catchup=False) as dag:
+    init_wrfv4_E = PythonOperator(
+        task_id='init_wrfv4E',
         provide_context=True,
         python_callable=run_this_func,
         pool=dag_pool
     )
 
-    running_state_wrfv4C = PythonOperator(
-        task_id='running_state_wrfv4C',
+    running_state_wrfv4E = PythonOperator(
+        task_id='running_state_wrfv4E',
         provide_context=True,
         python_callable=set_running_status,
         pool=dag_pool
     )
 
-    check_gfs_availability_wrfv4C = GfsSensorOperator(
-        task_id='check_gfs_availability_wrfv4C',
+    check_gfs_availability_wrfv4E = GfsSensorOperator(
+        task_id='check_gfs_availability_wrfv4E',
         poke_interval=60,
         timeout=60 * 30,
-        params={'model': 'C', 'init_task_id': 'init_wrfv4C'},
+        params={'model': 'E', 'init_task_id': 'init_wrfv4E'},
         provide_context=True,
         pool=dag_pool
     )
 
-    run_wrf4_C = BashOperator(
-        task_id='run_wrf4_C',
-        bash_command=run_wrf4_C_cmd,
+    run_wrf4_E = BashOperator(
+        task_id='run_wrf4_E',
+        bash_command=run_wrf4_E_cmd,
         pool=dag_pool
     )
 
-    rfield_gen_wrfv4C = BashOperator(
-        task_id='rfield_gen_wrfv4C',
+    rfield_gen_wrfv4E = BashOperator(
+        task_id='rfield_gen_wrfv4E',
         bash_command=rfield_gen_cmd,
         pool=dag_pool
     )
 
-    wrf_data_push_wrfv4C = BashOperator(
-        task_id='wrf_data_push_wrfv4C',
+    wrf_data_push_wrfv4E = BashOperator(
+        task_id='wrf_data_push_wrfv4E',
         bash_command=data_push_cmd,
         pool=dag_pool
     )
 
-    check_accuracy_wrfv4C = PythonOperator(
-        task_id='check_accuracy_wrfv4C',
+    check_accuracy_wrfv4E = PythonOperator(
+        task_id='check_accuracy_wrfv4E',
         provide_context=True,
         python_callable=check_accuracy,
         pool=dag_pool
     )
 
-    complete_state_wrfv4C = PythonOperator(
-        task_id='complete_state_wrfv4C',
+    complete_state_wrfv4E = PythonOperator(
+        task_id='complete_state_wrfv4E',
         provide_context=True,
         python_callable=set_complete_status,
         pool=dag_pool
     )
 
-    init_wrfv4_C >> running_state_wrfv4C >> check_gfs_availability_wrfv4C >> \
-    run_wrf4_C >> rfield_gen_wrfv4C >> wrf_data_push_wrfv4C >> \
-    check_accuracy_wrfv4C >> complete_state_wrfv4C
+    init_wrfv4_E >> running_state_wrfv4E >> check_gfs_availability_wrfv4E >> \
+    run_wrf4_E >> rfield_gen_wrfv4E >> wrf_data_push_wrfv4E >> \
+    check_accuracy_wrfv4E >> complete_state_wrfv4E
