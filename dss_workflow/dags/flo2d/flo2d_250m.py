aab8f57ffd20a8a26990390fd33f67860cd447f2
hasitha <hasithadkr7@gmail.com>
2020-01-01 12:40:11 +0530

    adding accuracy checking condition to flo2d 250m dag

--- dss_workflow/dags/flo2d/flo2d_250m.py
+++ dss_workflow/dags/flo2d/flo2d_250m.py
@@ -104,8 +104,11 @@ def check_accuracy(**context):
     print('check_accuracy|flo2d_rule : ', flo2d_rule)
     exec_date = context["execution_date"].to_datetime_string()
     print('check_accuracy|exec_date : ', flo2d_rule)
-    # TODO: condition tobe added
-    calculate_flo2d_rule_accuracy(flo2d_rule, exec_date)
+    accuracy_rule_id = rule_info['accuracy_rule']
+    if accuracy_rule_id == 0 or accuracy_rule_id == '0':
+        return True
+    else:
+        calculate_flo2d_rule_accuracy(flo2d_rule, exec_date)
 
 
 def update_workflow_status(status, rule_id):
