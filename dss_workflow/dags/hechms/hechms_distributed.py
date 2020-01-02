e63817e28b5e8b0ee8ea3f632af7fc56509bcf59
hasitha <hasithadkr7@gmail.com>
2020-01-02 10:22:42 +0530

    get_local_exec_date_from_context update in distributed hechms

--- dss_workflow/dags/hechms/hechms_distributed.py
+++ dss_workflow/dags/hechms/hechms_distributed.py
@@ -38,8 +38,7 @@ def get_rule_from_context(context):
 
 def get_local_exec_date_from_context(context):
     exec_datetime_str = context["execution_date"].to_datetime_string()
-    exec_datetime = datetime.strptime(exec_datetime_str, '%Y-%m-%d %H:%M:%S') \
-                    - timedelta(days=1) + timedelta(hours=5, minutes=30)
+    exec_datetime = datetime.strptime(exec_datetime_str, '%Y-%m-%d %H:%M:%S') + timedelta(hours=5, minutes=30)
     exec_date = exec_datetime.strftime('%Y-%m-%d_%H:00:00')
     return exec_date
 
