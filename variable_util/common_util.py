956e71b14608830810196d977e8f49dfff1c169c
hasitha <hasithadkr7@gmail.com>
2020-01-06 14:16:11 +0530

    search_in_dictionary_list

--- variable_util/common_util.py
+++ variable_util/common_util.py
@@ -14,3 +14,10 @@ def get_iteration_gap_of_cron_exp(cron_exp):
     print([d[0], h[0], m[0], s])
     return {'days': d[0], 'hours': h[0], 'minutes': m[0], 'seconds': s}
 
+
+# search in list of dictionaries
+def search_in_dictionary_list(input_list, key):
+    for item in input_list:
+        if item[key]:
+            return item
+    return None
