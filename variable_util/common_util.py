a477a17df035b4d876e9e55e8dbe8b8c06a18e66
hasitha <hasithadkr7@gmail.com>
2020-01-06 10:21:32 +0530

    get time gap in days,hours,minutes and seconds of a cron expression

---
+++ variable_util/common_util.py
@@ -0,0 +1,15 @@
+import croniter
+from datetime import datetime
+
+
+def get_iteration_gap_of_cron_exp(cron_exp):
+    cron = croniter.croniter(cron_exp, datetime.now())
+    date1 = cron.get_next(datetime)
+    date2 = cron.get_next(datetime)
+    difference = (date2 - date1).total_seconds()
+    d = divmod(difference, 86400)  # days
+    h = divmod(d[1], 3600)  # hours
+    m = divmod(h[1], 60)  # minutes
+    s = m[1]  # seconds
+    print([d[0], h[0], m[0], s])
+    return {'days': d[0], 'hours': h[0], 'minutes': m[0], 'seconds': s}
