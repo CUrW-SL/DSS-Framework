888bd41c13b4d6a3fcf8ecb06cb85c67d285b847
hasitha <hasithadkr7@gmail.com>
2020-01-09 20:23:46 +0530

    get_time_gap_of_two_times

--- variable_util/common_util.py
+++ variable_util/common_util.py
@@ -6,13 +6,7 @@ def get_iteration_gap_of_cron_exp(cron_exp):
     cron = croniter.croniter(cron_exp, datetime.now())
     date1 = cron.get_next(datetime)
     date2 = cron.get_next(datetime)
-    difference = (date2 - date1).total_seconds()
-    d = divmod(difference, 86400)  # days
-    h = divmod(d[1], 3600)  # hours
-    m = divmod(h[1], 60)  # minutes
-    s = m[1]  # seconds
-    print([d[0], h[0], m[0], s])
-    return {'days': d[0], 'hours': h[0], 'minutes': m[0], 'seconds': s}
+    return get_time_gap_of_two_times(date2, date1)
 
 
 # search in list of dictionaries
@@ -31,3 +25,13 @@ def lower_time_limit(current_time, cron_exp):
                                            seconds=time_gap['seconds'])
     print('lower_time_limit|lower_limit : ', lower_limit)
     return lower_limit
+
+
+def get_time_gap_of_two_times(time1, time2):
+    difference = (time1 - time2).total_seconds()
+    d = divmod(difference, 86400)  # days
+    h = divmod(d[1], 3600)  # hours
+    m = divmod(h[1], 60)  # minutes
+    s = m[1]  # seconds
+    print([d[0], h[0], m[0], s])
+    return {'days': d[0], 'hours': h[0], 'minutes': m[0], 'seconds': s}
