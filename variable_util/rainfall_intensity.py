afc934b4074e8bcd72c43d6d71f9ca1518c6a51e
hasitha <hasithadkr7@gmail.com>
2020-01-10 19:32:07 +0530

    rainfall dataframe validation

--- variable_util/rainfall_intensity.py
+++ variable_util/rainfall_intensity.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pandas as pd
 
 DEFAULT_VARIABLE_VALUE = -9999
+TIME_STEPS_FOR_HOUR = 12
 
 sys.path.insert(0, '/home/uwcc-admin/git/DSS-Framework/variable_util')
 from common_util import get_time_gap_of_two_times, search_in_dictionary_list, lower_time_limit
@@ -17,18 +18,19 @@ def update_rainfall_intensity_values(dss_adapter, obs_adapter, variable_routine)
     current_time = datetime.now()
     print('update_rainfall_intensity_values|current_time : ', current_time)
     for variable_value in variable_values:
-        rainfall_values = pd.DataFrame(data=variable_value['results'].reverse(),
-                                       columns=['time', 'value']).set_index(keys='time')
+        rainfall_values = pd.DataFrame(data=variable_value['results'].reverse(), columns=['time', 'value'])
         print('update_current_rainfall_values|rainfall_values : ', rainfall_values)
+        validate_rainfall_values(rainfall_values)
 
 
 def validate_rainfall_values(rainfall_values):
-    if len(rainfall_values) == 12:
-        start_time = rainfall_values[0][0]
-        end_time = rainfall_values[11][0]
+    row_count = rainfall_values.shape()[0]
+    if row_count == TIME_STEPS_FOR_HOUR:
+        start_time = rainfall_values['time'].iloc[0]
+        print('validate_rainfall_values|start_time : ', start_time)
+        end_time = rainfall_values['time'].iloc[-1]
         time_gap = get_time_gap_of_two_times(end_time, start_time)
-        if {'days': 0, 'hours': 1, 'minutes': 0, 'seconds': 0} == time_gap:
-            print('')
+        print('validate_rainfall_values|time_gap : ', time_gap)
     else:
         return False
 
