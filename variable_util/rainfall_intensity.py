d24fba10855e8b2dfe2122c38cc1c1565be50684
hasitha <hasithadkr7@gmail.com>
2020-01-10 19:21:56 +0530

    formatting rainfall dataframe

--- variable_util/rainfall_intensity.py
+++ variable_util/rainfall_intensity.py
@@ -17,7 +17,8 @@ def update_rainfall_intensity_values(dss_adapter, obs_adapter, variable_routine)
     current_time = datetime.now()
     print('update_rainfall_intensity_values|current_time : ', current_time)
     for variable_value in variable_values:
-        rainfall_values = pd.DataFrame(variable_value['results'].reverse(), columns=['time', 'value'])
+        rainfall_values = pd.DataFrame(data=variable_value['results'].reverse(),
+                                       columns=['time', 'value']).set_index(keys='time')
         print('update_current_rainfall_values|rainfall_values : ', rainfall_values)
 
 
