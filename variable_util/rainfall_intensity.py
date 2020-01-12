e3452610ed0e7300be30b3cc517afb33b729b608
hasitha <hasithadkr7@gmail.com>
2020-01-12 09:04:59 +0530

    validate_rainfall_values issue fix

--- variable_util/rainfall_intensity.py
+++ variable_util/rainfall_intensity.py
@@ -26,9 +26,10 @@ def update_rainfall_intensity_values(dss_adapter, obs_adapter, variable_routine)
 def validate_rainfall_values(rainfall_values):
     row_count = rainfall_values.shape[0]
     if row_count == TIME_STEPS_FOR_HOUR:
-        start_time = rainfall_values['time'].iloc[0]
+        start_time = rainfall_values['time'].iloc[-1]
         print('validate_rainfall_values|start_time : ', start_time)
-        end_time = rainfall_values['time'].iloc[-1]
+        end_time = rainfall_values['time'].iloc[0]
+        print('validate_rainfall_values|end_time : ', end_time)
         time_gap = get_time_gap_of_two_times(end_time, start_time)
         print('validate_rainfall_values|time_gap : ', time_gap)
     else:
