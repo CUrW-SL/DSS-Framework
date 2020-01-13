8147d522afb49bf07f4b1796f822b4823ff75275
hasitha <hasithadkr7@gmail.com>
2020-01-13 18:11:17 +0530

    implementing evaluate_configuration_logics

--- rule_engine/rule_executor.py
+++ rule_engine/rule_executor.py
@@ -1,6 +1,12 @@
-def get_next_pump_configurations(routines):
+import sys
+
+sys.path.insert(0, '/home/uwcc-admin/git/DSS-Framework/gen_util')
+from controller_util import is_matched
+
+
+def get_next_pump_configurations(dss_adapter, routines):
     dag_info = []
-    success_routines = evaluate_configuration_logics(routines)
+    success_routines = evaluate_configuration_logics(dss_adapter, routines)
     if len(success_routines) > 0:
         for success_routine in success_routines:
             dag_name = success_routine['dag_name']
@@ -15,10 +21,15 @@ def get_next_pump_configurations(routines):
 # or
 # ((location_name='Kohuwala') and (variable_type='Precipitation') and ((rainfall_intensity>=65.4) or ((last_1_day_rainfall>=150) and (last_3_day_rainfall>=420))))
 
-def evaluate_configuration_logics(routines):
+def evaluate_configuration_logics(dss_adapter, routines):
     print('evaluate_configuration_logics|routines : ', routines)
     passed_routines = []
     for routine in routines:
         rule_logic = routine['rule_logic']
-        print('evaluate_configuration_logics|rule_logic : ', rule_logic)
-    return routines
+        if is_matched(rule_logic):
+            print('evaluate_configuration_logics|rule_logic : ', rule_logic)
+            location_names = dss_adapter.evaluate_variable_rule_logic(rule_logic)
+            if location_names is not None and len(location_names) > 0:
+                passed_routines.append(routine)
+    print('evaluate_configuration_logics|passed_routines : ', passed_routines)
+    return passed_routines
