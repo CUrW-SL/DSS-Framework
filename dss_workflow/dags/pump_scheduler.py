e7f071d6ade77361bafbf95a299547efb19fcb8e
hasitha <hasithadkr7@gmail.com>
2020-01-13 14:32:20 +0530

    modify scheduler dag to get successful routines to trigger

--- dss_workflow/dags/pump_scheduler.py
+++ dss_workflow/dags/pump_scheduler.py
@@ -12,12 +12,12 @@ from dynamic_external_trigger_operator import DynamicTriggerDagRunOperator
 sys.path.insert(0, '/home/uwcc-admin/git/DSS-Framework/db_util')
 from dss_db import RuleEngineAdapter
 
-sys.path.insert(0, '/home/uwcc-admin/git/DSS-Framework/gen_util')
-from controller_util import get_triggering_variable_dags
+sys.path.insert(0, '/home/uwcc-admin/git/DSS-Framework/rule_engine')
+from rule_executor import get_next_pump_configurations
 
-prod_dag_name = 'variable_scheduler_v1'
+prod_dag_name = 'pump_scheduler_v1'
 schedule_interval = '*/5 * * * *'
-dag_pool = 'variable_scheduler_pool'
+dag_pool = 'pump_scheduler_pool'
 
 
 def generate_dag_run(context):
@@ -26,11 +26,11 @@ def generate_dag_run(context):
     adapter = RuleEngineAdapter.get_instance(db_config)
     run_date = context["execution_date"].to_datetime_string()
     print('init_variable_routine|run_date : ', run_date)
-    routines = adapter.get_next_variable_routines(run_date)
+    routines = adapter.get_next_pump_routines(run_date)
     print('init_variable_routine|routines : ', routines)
     next_variable_routines = []
     if routines is not None:
-        next_variable_routines = get_triggering_variable_dags(routines)
+        next_variable_routines = get_next_pump_configurations(routines)
     else:
         print('No variable routine to schedule.')
     return next_variable_routines
--- rule_engine/rule_executor.py
+++ rule_engine/rule_executor.py
@@ -1,3 +1,16 @@
+def get_next_pump_configurations(routines):
+    dag_info = []
+    success_routines = evaluate_configuration_logics(routines)
+    if len(success_routines) > 0:
+        for success_routine in success_routines:
+            dag_name = success_routine['dag_name']
+            payload = success_routine
+            dag_info.append({'dag_name': dag_name, 'payload': payload})
+    else:
+        print('No triggering_pump_dags found.')
+    return dag_info
 
 
-
+def evaluate_configuration_logics(routines):
+    print('evaluate_configuration_logics|routines : ', routines)
+    return routines
