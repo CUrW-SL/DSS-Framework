9b70a0f8c8dc5ed00801dfb0a4e2853f2b33792d
hasitha <hasithadkr7@gmail.com>
2020-01-18 03:38:16 +0530

    search_value_in_dictionary_list

--- weather_models/flo2d/utils.py
+++ weather_models/flo2d/utils.py
@@ -5,4 +5,12 @@ def search_in_dictionary_list(dictionary_list, key_name, key_value):
     for dictionary in dictionary_list:
         if dictionary[key_name] == key_value:
             return dictionary
-    return INVALID_VALUE
+    return None
+
+
+def search_value_in_dictionary_list(dictionary_list, key_name, key_value, value_key):
+    dictionary = search_in_dictionary_list(dictionary_list, key_name, key_value)
+    if dictionary is not None:
+        return dictionary[value_key]
+    else:
+        return INVALID_VALUE
