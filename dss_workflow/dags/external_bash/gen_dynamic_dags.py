01059bdabccd1decbed4da3fba204247e986ace7
hasitha <hasithadkr7@gmail.com>
2020-01-21 14:13:28 +0530

    update set_running_status and set_complete_status methods

--- dss_workflow/dags/external_bash/gen_dynamic_dags.py
+++ dss_workflow/dags/external_bash/gen_dynamic_dags.py
@@ -36,7 +36,7 @@ def update_workflow_status(status, rule_id):
         print('update_workflow_status|db_config|Exception: ', str(e))
 
 
-def set_running_status(**context):
+def set_running_status(context):
     print('set_running_status|context : ', context)
     routine_id = get_rule_id(context)
     print('set_running_status|routine_id :', routine_id)
@@ -46,7 +46,7 @@ def set_running_status(**context):
         print('set_running_status|rule_id not found')
 
 
-def set_complete_status(**context):
+def set_complete_status(context):
     print('set_complete_status|context : ', context)
     routine_id = get_rule_id(context)
     print('set_complete_status|routine_id :', routine_id)
