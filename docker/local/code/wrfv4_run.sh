7212b70251d8ea74131a037d9b1e04a3869b09bb
hasitha <hasithadkr7@gmail.com>
2020-01-26 21:56:21 +0530

    NAMELIST_WPS_CONTENT NAMELIST_INPUT_CONTENT passing in docker entry point

--- docker/local/code/wrfv4_run.sh
+++ docker/local/code/wrfv4_run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 echo "#### Reading running args..."
-while getopts ":d:i:m:g:k:v:" option; do
+while getopts ":d:i:m:g:k:v:a:b:" option; do
   case "${option}" in
 
   d) START_DATE=$OPTARG ;;
   k) RUN_ID=$OPTARG ;;
   m) MODE=$OPTARG ;;
+  a) NAMELIST_WPS_CONTENT=$OPTARG ;;
+  b) $NAMELIST_INPUT_CONTENT=$OPTARG ;;
   esac
 done
 
@@ -15,6 +17,8 @@ check_empty() {
 }
 echo "START_DATE : $START_DATE"
 echo "RUN_ID : $RUN_ID"
+echo "MODE : $MODE"
+echo "MODE : $MODE"
 echo "MODE : $MODE"
 echo "#### Running WRF procedures..."
 
@@ -26,4 +30,6 @@ python3 wrfv4_run.py \
                     $( check_empty "$START_DATE" start_date ) \
                     $( check_empty "$MODE" mode ) \
                     $( check_empty "$RUN_ID" run_id )
+                    $( check_empty "$NAMELIST_WPS_CONTENT" wps_content )
+                    $( check_empty "$NAMELIST_INPUT_CONTENT" input_content )
 echo "####WRF procedures completed"
