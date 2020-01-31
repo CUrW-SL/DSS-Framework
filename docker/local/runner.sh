7378b2adc92f2af163aaf1c757747bff09f80c27
hasitha <hasithadkr7@gmail.com>
2020-01-31 09:36:29 +0530

    get user inputs to bash

--- docker/local/runner.sh
+++ docker/local/runner.sh
@@ -2,7 +2,7 @@
 
 echo "#### Running WRF procedures..."
 
-mkdir -p /mnt/disks/data/wrf-data/gfs
+#mkdir -p /mnt/disks/data/wrf-data/gfs
 
 while getopts ":r:m:v:d:h:a:b:C:" option; do
   case "${option}" in
@@ -14,6 +14,8 @@ while getopts ":r:m:v:d:h:a:b:C:" option; do
   a) NAMELIST_WPS_ID=$OPTARG ;; # 1
   b) NAMELIST_INPUT_ID=$OPTARG ;; # 2
   c) DB_CONFIG=$OPTARG ;; # 2
+  \?) echo "Invalid option: $OPTARG" ;;
+  :) echo "Invalid option: $OPTARG requires an argument" ;;
   esac
 done
 
