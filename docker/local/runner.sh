f03259b36d1d21fc7bafd175d19f3c28a05c9638
hasitha <hasithadkr7@gmail.com>
2020-01-31 12:18:34 +0530

    docker run command issue fix

--- docker/local/runner.sh
+++ docker/local/runner.sh
@@ -62,7 +62,7 @@ echo "exec_date : ${exec_date}"
 echo "wrf_id : ${wrf_id}"
 echo "docker_tag : ${docker_tag}"
 
-docker run -i --rm --privileged
+docker run -i --rm --privileged \
     -v /mnt/disks/data/wrf-data/nfs:/home/Build_WRF/nfs \
     -v /mnt/disks/data/wrf-data/gfs:/home/Build_WRF/gfs \
     -v /mnt/disks/data/wrf-data/archive:/home/Build_WRF/archive \
