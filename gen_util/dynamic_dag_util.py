1821852c706d7c84177a3cdf1106af2e7f48e068
hasitha <hasithadkr7@gmail.com>
2020-02-05 18:05:50 +0530

    dynamic dag generation

---
+++ gen_util/dynamic_dag_util.py
@@ -0,0 +1,35 @@
+import json
+
+
+def get_all_dynamic_dag_routines(dss_adapter):
+    query = 'select id, dag_name, schedule, timeout from dss.dynamic_routine;'
+    print('get_all_dynamic_dag_routines|query : ', query)
+    results = dss_adapter.get_multiple_result(query)
+    routines = []
+    if results is not None:
+        for result in results:
+            print('get_all_external_bash_routines|result : ', result)
+            routines.append({'id': result[0], 'dag_name': result[1], 'schedule': result[2],
+                             'timeout': json.loads(result[3])})
+    print('get_all_external_bash_routines|routines : ', routines)
+    return routines
+
+
+def get_dynamic_dag_tasks(dss_adapter, dag_id):
+    sql_query = 'select id, task_name, task_type, task_content, input_params, timeout ' \
+                'from dss.dynamic_workflow where active=1 and owner_dag_id={} order by task_order asc;'.format(dag_id)
+    print('get_dynamic_dag_tasks|sql_query : ', sql_query)
+    results = dss_adapter.get_multiple_result(sql_query)
+    print('get_dynamic_dag_tasks|results : ', results)
+    dag_tasks = []
+    if results is not None:
+        for result in results:
+            if result[3]:
+                dag_tasks.append({'id': result[0], 'task_name': result[1], 'task_type': result[2], 'task_content': result[3],
+                                  'input_params': json.loads(result[4]), 'timeout': json.loads(result[5])})
+            else:
+                dag_tasks.append({'id': result[0], 'task_name': result[1], 'task_type': result[2],
+                                  'task_content': result[3], 'input_params': result[4], 'timeout': json.loads(result[5])})
+    print('get_dynamic_dag_tasks|dag_tasks : ', dag_tasks)
+    return dag_tasks
+
