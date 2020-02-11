9a126e7cf3955af08953e57b8b0d67cf6b1d9b00
hasitha <hasithadkr7@gmail.com>
2020-02-12 04:25:48 +0530

    remove accuracy checking task from flo2d 150m dag

--- local_dss_workflow/dags/flo2d/flo2d_250m.py
+++ local_dss_workflow/dags/flo2d/flo2d_250m.py
@@ -281,12 +281,12 @@ with DAG(dag_id=prod_dag_name, default_args=default_args, schedule_interval=None
     #     pool=dag_pool
     # )
 
-    check_accuracy_flo2d250m = PythonOperator(
-        task_id='check_accuracy_flo2d250m',
-        provide_context=True,
-        python_callable=check_accuracy,
-        pool=dag_pool
-    )
+    # check_accuracy_flo2d250m = PythonOperator(
+    #     task_id='check_accuracy_flo2d250m',
+    #     provide_context=True,
+    #     python_callable=check_accuracy,
+    #     pool=dag_pool
+    # )
 
     complete_state_flo2d_250m = PythonOperator(
         task_id='complete_state',
@@ -297,6 +297,7 @@ with DAG(dag_id=prod_dag_name, default_args=default_args, schedule_interval=None
 
     init_flo2d_250m >> running_state_flo2d_250m >> create_raincell_flo2d_250m >> create_chan_flo2d_250m >> \
     create_inflow_flo2d_250m >>  create_outflow_flo2d_250m >> run_flo2d_250m_flo2d_250m >> \
-    check_accuracy_flo2d250m >> complete_state_flo2d_250m
+    complete_state_flo2d_250m
+    # check_accuracy_flo2d250m >> complete_state_flo2d_250m
     # extract_water_level_flo2d_250m >> check_accuracy_flo2d250m >> complete_state_flo2d_250m
 
