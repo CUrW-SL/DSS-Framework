e32aa44efaf5adc96491d5fa41f1a3ac8439a444
hasitha <hasithadkr7@gmail.com>
2020-02-12 18:43:43 +0530

    sql sensor query update

--- local_dss_workflow/dags/dynamic_dag/dag_generation.py
+++ local_dss_workflow/dags/dynamic_dag/dag_generation.py
@@ -185,9 +185,9 @@ def get_sensor_sql_query(model_type, model_rule_id):
     print('get_sensor_sql_query|[model_type, model_rule_id] : ', [model_type, model_rule_id])
     sql_query = None
     if model_type == 'hechms':
-        sql_query = 'select TRUE from dss.hechms_rules where id={} and status=3'.format(model_rule_id)
+        sql_query = 'select TRUE from dss.hechms_rules where id={} and status in (3, 4)'.format(model_rule_id)
     elif model_type == 'flo2d':
-        sql_query = 'select TRUE from dss.flo2d_rules where id={} and status=3'.format(model_rule_id)
+        sql_query = 'select TRUE from dss.flo2d_rules where id={} and status in (3, 4)'.format(model_rule_id)
     return sql_query
 
 
