bf317ba71971304e63f5ae979ccc1ede78d3e256
hasitha <hasithadkr7@gmail.com>
2020-03-04 15:21:02 +0530

    flo2d dag issue

--- alpha_workflow/dag/flo2d/flo2d_dag.py
+++ alpha_workflow/dag/flo2d/flo2d_dag.py
@@ -365,7 +365,7 @@ def get_dss_db_adapter():
 
 
 def get_rule_id(context):
-    rule_info = context['task_instance'].xcom_pull(task_ids='init_flo2d')['rule_info']
+    rule_info = context['task_instance'].xcom_pull(task_ids='init_flo2d')
     if rule_info:
         rule_id = rule_info['id']
         print('get_rule_id|rule_id : ', rule_id)
