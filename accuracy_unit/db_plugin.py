fd0e5b6a55891fbf8c481685629b7c32c32112c4
hasitha <hasithadkr7@gmail.com>
2020-04-14 12:11:29 +0530

    mean calculation in obs and fcst

--- accuracy_unit/db_plugin.py
+++ accuracy_unit/db_plugin.py
@@ -1,4 +1,6 @@
 from decimal import Decimal
+import pymysql
+from mysql.connector import cursor
 from scipy.spatial import Voronoi
 from shapely.geometry import Polygon, Point, shape
 import geopandas as gpd
@@ -19,6 +21,14 @@ def get_multiple_result(db_connection, query):
     cur.execute(query)
     rows = cur.fetchall()
     return rows
+
+
+def get_latest_fgt(fcst_connection, hash_id, start_time):
+    cur = fcst_connection.cursor()
+    cur.callproc('getLatestFGTs', (hash_id, start_time))
+    rows = cur.fetchall()
+    print('get_latest_fgts|rows: ', rows)
+    return rows[0]['fgt'].strftime('%Y-%m-%d %H:%M:%S')
 
 
 def is_inside_basin(shape_file, latitude, longitude):
@@ -52,9 +62,81 @@ def get_wrf_station_hash_ids(fcst_connection, sim_tag, wrf_model_id, basin_point
     return hash_ids
 
 
-def get_station_timeseries(fcst_connection, hash_id, start_time, end_time):
+def get_station_timeseries(fcst_connection, hash_id, latest_fgt, start_time, end_time):
     sql_query = 'select time,value from curw_fcst.data where id=\'{}\' and time>=\'{}\' ' \
-                'and time<=\'{}\''.format(hash_id, start_time, end_time)
+                'and time<\'{}\' and fgt=\'{}\''.format(hash_id, start_time, end_time, latest_fgt)
     rows = get_multiple_result(fcst_connection, sql_query)
-    df = pd.DataFrame(data=rows, columns=['time', 'value']).set_index(keys='time')
-    return df
+    df = pd.DataFrame(data=rows, columns=['time', 'value'])
+    df['time'] = pd.to_datetime(df['time'])
+    resample_df = (df.set_index('time').resample('5T').first().reset_index().reindex(columns=df.columns))
+    interpolate_df = resample_df.interpolate(method='linear', limit_direction='forward')
+    interpolate_df['value'].fillna(Decimal(0.0), inplace=True)
+    return interpolate_df.set_index(keys='time')
+
+
+def get_obs_basin_stations(obs_connection, shape_file):
+    basin_points = []
+    sql_query = 'select id,latitude,longitude from curw_obs.station where station_type=\'CUrW_WeatherStation\' ' \
+                'and longitude between 79.6 and 81.0;'
+    rows = get_multiple_result(obs_connection, sql_query)
+    for row in rows:
+        latitude = Decimal(row['latitude'])
+        longitude = Decimal(row['longitude'])
+        if is_inside_basin(shape_file, latitude, longitude):
+            basin_points.append(row['id'])
+    return basin_points
+
+
+def get_obs_station_hash_ids(obs_connection, basin_points, start_time):
+    hash_ids = []
+    basin_points_str = ','.join(str(x) for x in basin_points)
+    sql_query = 'select id from curw_obs.run where variable=10 and unit=9 and ' \
+                'station in ({}) and end_date>\'{}\';'.format(basin_points_str,start_time)
+    rows = get_multiple_result(obs_connection, sql_query)
+    for row in rows:
+        hash_ids.append(row['id'])
+    return hash_ids
+
+
+def get_obs_station_timeseries(obs_connection, hash_id, timeseries_start, timeseries_end, max_error):
+    data_sql = 'select time,value from curw_obs.data where id=\'{}\' and time >= \'{}\' ' \
+               'and time <= \'{}\''.format(hash_id, timeseries_start, timeseries_end)
+    try:
+        results = get_multiple_result(obs_connection, data_sql)
+        if len(results) > 0:
+            time_step_count = int((datetime.strptime(timeseries_end, '%Y-%m-%d %H:%M:%S')
+                                   - datetime.strptime(timeseries_start, '%Y-%m-%d %H:%M:%S')).total_seconds() / (
+                                              60 * 5))
+            data_error = ((time_step_count - len(results)) / time_step_count)
+            if data_error < 0:
+                df = pd.DataFrame(data=results, columns=['time', 'value']).set_index(keys='time')
+                return df
+            elif data_error < max_error:
+                print('data_error : {}'.format(data_error))
+                print('filling missing data.')
+                formatted_ts = []
+                i = 0
+                for step in range(time_step_count + 1):
+                    tms_step = datetime.strptime(timeseries_start, '%Y-%m-%d %H:%M:%S') + timedelta(
+                        minutes=step * 5)
+                    if step < len(results):
+                        if tms_step == results[i]['time']:
+                            formatted_ts.append(results[i])
+                        else:
+                            formatted_ts.append({'time': tms_step, 'value': Decimal(0)})
+                    else:
+                        formatted_ts.append({'time': tms_step, 'value': Decimal(0)})
+                    i += 1
+                df = pd.DataFrame(data=formatted_ts, columns=['time', 'value']).set_index(keys='time')
+                print('get_station_timeseries|df: ', df)
+                return df
+            else:
+                print('data_error : {}'.format(data_error))
+                print('Data error is too large')
+                return None
+        else:
+            print('No data.')
+            return None
+    except Exception as e:
+        print('get_timeseries_by_id|data fetch|Exception:', e)
+        return None
--- accuracy_unit/wrf/mean_calc.py
+++ accuracy_unit/wrf/mean_calc.py
@@ -1,7 +1,8 @@
 import pymysql
 import os
 from accuracy_unit.db_plugin import get_wrf_basin_stations, \
-    get_wrf_station_hash_ids, get_station_timeseries
+    get_wrf_station_hash_ids, get_station_timeseries, get_latest_fgt, \
+    get_obs_basin_stations, get_obs_station_hash_ids, get_obs_station_timeseries
 
 RESOURCE_PATH = '/home/hasitha/PycharmProjects/DSS-Framework/resources/shape_files'
 
@@ -18,22 +19,53 @@ PORT = 3306
 def calculate_wrf_model_mean(sim_tag, wrf_model, start_time, end_time):
     print('calculate_wrf_model_mean|[sim_tag, wrf_model, start_time, end_time]: ',
           [sim_tag, wrf_model, start_time, end_time])
-    sim_connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=SIM_DB,
-                                     cursorclass=pymysql.cursors.DictCursor)
     fcst_connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=FCST_DB,
                                       cursorclass=pymysql.cursors.DictCursor)
     obs_connection = pymysql.connect(host=HOST, user=USER, password=PASSWORD, db=OBS_DB,
                                      cursorclass=pymysql.cursors.DictCursor)
     # shape_file = os.path.join(RESOURCE_PATH, 'klb-wgs84/klb-wgs84.shp')
     shape_file = os.path.join(RESOURCE_PATH, 'Kalani_basin_hec_wgs/Kalani_basin_hec_wgs.shp')
+    get_obs_cum_mean_df(obs_connection, shape_file, start_time, end_time)
+    get_fcst_cum_mean_df(fcst_connection, shape_file, sim_tag, wrf_model, start_time, end_time)
+
+
+def get_obs_cum_mean_df(obs_connection, shape_file, start_time, end_time, max_error=0.7):
+    basin_points = get_obs_basin_stations(obs_connection, shape_file)
+    total_df = None
+    station_count = 0
+    if len(basin_points) > 0:
+        print(basin_points)
+        hash_ids = get_obs_station_hash_ids(obs_connection, basin_points, start_time)
+        for hash_id in hash_ids:
+            df = get_obs_station_timeseries(obs_connection, hash_id, start_time, end_time, max_error)
+            print('get_obs_cum_mean_df|df : ', df)
+            if df is not None:
+                if station_count == 0:
+                    total_df = df
+                else:
+                    total_df = total_df.add(df, fill_value=0)
+                station_count += 1
+        if total_df is not None:
+            print('get_obs_cum_mean_df|total_df : ', total_df)
+            obs_mean_df = total_df['value'] / station_count
+            obs_cum_mean_df = obs_mean_df.cumsum()
+            print('get_obs_cum_mean_df|obs_cum_mean_df : ', obs_cum_mean_df)
+            obs_cum_mean_df.to_csv('/home/hasitha/PycharmProjects/DSS-Framework/output/obs_cum_mean_df.csv')
+            return obs_cum_mean_df
+
+
+def get_fcst_cum_mean_df(fcst_connection, shape_file, sim_tag, wrf_model, start_time, end_time):
     basin_points = get_wrf_basin_stations(fcst_connection, shape_file)
     total_df = None
     station_count = 0
     if len(basin_points) > 0:
         print(basin_points)
         hash_ids = get_wrf_station_hash_ids(fcst_connection, sim_tag, wrf_model, basin_points)
+        latest_fgt = get_latest_fgt(fcst_connection, hash_ids[0], start_time)
+        print('calculate_wrf_model_mean|latest_fgt : ', latest_fgt)
         for hash_id in hash_ids:
-            df = get_station_timeseries(fcst_connection, hash_id, start_time, end_time)
+            df = get_station_timeseries(fcst_connection, hash_id, latest_fgt, start_time, end_time)
+            print('calculate_wrf_model_mean|df : ', df)
             if df is not None:
                 if station_count == 0:
                     total_df = df
@@ -43,9 +75,11 @@ def calculate_wrf_model_mean(sim_tag, wrf_model, start_time, end_time):
         if total_df is not None:
             print('calculate_wrf_model_mean|total_df : ', total_df)
             total_df.to_csv('/home/hasitha/PycharmProjects/DSS-Framework/output/rain.csv')
-            mean_df = total_df['value'] / station_count
-            cum_mean_df = mean_df.cumsum()
-            print('calculate_wrf_model_mean|cum_mean_df : ', cum_mean_df)
+            fcst_mean_df = total_df['value'] / station_count
+            fcst_cum_mean_df = fcst_mean_df.cumsum()
+            print('calculate_wrf_model_mean|fcst_cum_mean_df : ', fcst_cum_mean_df)
+            fcst_cum_mean_df.to_csv('/home/hasitha/PycharmProjects/DSS-Framework/output/fcst_cum_mean_df.csv')
+            return fcst_cum_mean_df
 
 
 if __name__ == '__main__':
