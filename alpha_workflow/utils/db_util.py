72be76161d212991d64b5eba92fa51156956caec
hasitha <hasithadkr7@gmail.com>
2020-04-20 20:03:32 +0530

    getting dynamic dag tasks to decision dag

--- alpha_workflow/utils/db_util.py
+++ alpha_workflow/utils/db_util.py
@@ -1032,7 +1032,7 @@ class RuleEngineAdapter:
 
     def get_parent_dag_tasks(self, dag_rule_id):
         #query = 'select input_params from dss.dynamic_workflow where owner_dag_id={}'.format(dag_rule_id)
-        query = 'select input_params from dss.dynamic_workflow where owner_dag_id={}'.format(1)
+        query = 'select task_content, input_params from dss.dynamic_workflow where owner_dag_id={}'.format(1)
         print('get_parent_dag_tasks|query: ', query)
         results = self.get_multiple_result(query)
         print('get_parent_dag_tasks|query: ', results)
