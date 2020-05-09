bed6bf24967cdab4475e18aa543d704493b1d7ec
hasitha <hasithadkr7@gmail.com>
2020-05-10 02:12:55 +0530

    hechms docker entry point script update for python libs

--- docker/hechms/hechms_run.sh
+++ docker/hechms/hechms_run.sh
@@ -10,13 +10,50 @@ echo "hec-hms is running..."
 cd /home/hec-dssvue201
 ./hec-dssvue.sh &
 #echo "hec-dssvue is running..."
-cd /home
 
-export AIRFLOW_HOME=/home/airflow
-#mkdir ./OUTPUT
+echo "Changing into /home/curw/git/distributed_hechms"
+cd /home/curw/git/distributed_hechms
+echo "Inside `pwd`"
 
-#workflow initdb
-#sleep 5
-#echo "Starting Airflow Webserver"
-#workflow webserver -p 8080 &
-/bin/bash
+
+# If no venv (python3 virtual environment) exists, then create one.
+if [ ! -d "venv" ]
+then
+    echo "Creating venv python3 virtual environment."
+    virtualenv -p python3 venv
+fi
+
+# Activate venv.
+echo "Activating venv python3 virtual environment."
+source venv/bin/activate
+
+# Install dependencies using pip.
+echo "Installing Pandas"
+pip install pandas
+echo "Installing netCDF4"
+pip install netCDF4
+echo "Installing Flask"
+pip install Flask
+echo "Installing Flask-JSON"
+pip install Flask-JSON
+echo "Installing pandas"
+pip install pandas
+echo "Installing geopandas"
+pip install geopandas
+echo "Installing shapely"
+pip install shapely
+echo "Installing scipy"
+pip install scipy
+echo "Installing mysql-connector"
+pip install mysql-connector
+echo "Installing shapely"
+pip install shapely
+echo "Installing numpy"
+pip install numpy
+echo "Installing data layer"
+pip install git+https://github.com/shadhini/curw_db_adapter.git
+touch hechms.log
+
+# Run rfield generating scripts
+echo "Running HecHns Controller.py"
+python controller.py >> hechms.log 2>&1
