b85664ebe17495619eca8183d17a4c822b761ca5
hasitha <hasithadkr7@gmail.com>
2020-05-10 02:15:21 +0530

    hechms docker entry point script update for python libs

--- docker/hechms/hechms_run.sh
+++ docker/hechms/hechms_run.sh
@@ -4,13 +4,6 @@ Xvfb :1 -screen 0 1024x768x24 &> xvfb.log &
 export DISPLAY=:1.0
 echo "Xvbf is running..."
 
-./hec-hms-421/hec-hms.sh &
-echo "hec-hms is running..."
-
-cd /home/hec-dssvue201
-./hec-dssvue.sh &
-#echo "hec-dssvue is running..."
-
 echo "Changing into /home/curw/git/distributed_hechms"
 cd /home/curw/git/distributed_hechms
 echo "Inside `pwd`"
