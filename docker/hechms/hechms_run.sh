832ac15c02f2e3806191cafa4324ce9b63a5788c
hasitha <hasithadkr7@gmail.com>
2020-05-11 19:14:20 +0530

    execute hechms container

--- docker/hechms/hechms_run.sh
+++ docker/hechms/hechms_run.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+while getopts ":d:f:b:r:p:" option; do
+  case "${option}" in
+  d) EXEC_DATE=$OPTARG ;; # 2019-09-24 10:30:00
+  f) FORWARD=$OPTARG ;; # 3
+  b) BACKWARD=$OPTARG ;; # 2
+  r) INIT_RUN=$OPTARG ;; # 0 or 1
+  p) POP_METHOD=$OPTARG ;; # MME
+  esac
+done
+
+check_empty() {
+  [ -z "$1" ] && echo "" || echo "-$2=$1"
+}
+echo "EXEC_DATE : $EXEC_DATE"
+echo "FORWARD : $FORWARD"
+echo "BACKWARD : $BACKWARD"
+echo "INIT_RUN : $INIT_RUN"
+echo "POP_METHOD : $POP_METHOD"
+
+
 Xvfb :1 -screen 0 1024x768x24 &> xvfb.log &
 export DISPLAY=:1.0
 echo "Xvbf is running..."
@@ -7,7 +27,6 @@ echo "Xvbf is running..."
 echo "Changing into /home/curw/git/distributed_hechms"
 cd /home/curw/git/distributed_hechms
 echo "Inside `pwd`"
-
 
 # If no venv (python3 virtual environment) exists, then create one.
 if [ ! -d "venv" ]
@@ -48,5 +67,14 @@ pip install git+https://github.com/shadhini/curw_db_adapter.git
 touch hechms.log
 
 # Run rfield generating scripts
-echo "Running HecHns Controller.py"
-python controller.py >> hechms.log 2>&1
+echo "Running HEC-HMS Model."
+python hechms_workflow.py \
+                    $( check_empty "$EXEC_DATE" run_datetime ) \
+                    $( check_empty "$FORWARD" forward ) \
+                    $( check_empty "$BACKWARD" backward ) \
+                    $( check_empty "$INIT_RUN" init_run ) \
+                    $( check_empty "$POP_METHOD" pop_method )
+echo "####HEC-HMS procedures completed"
+
+deactivate
+echo "####HEC-HMS venv deactivated"
