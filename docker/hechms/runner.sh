611ecc83b8e1689b637fd9d360763a17bcd99be5
hasitha <hasithadkr7@gmail.com>
2020-05-11 19:16:04 +0530

    add runner script to hechms container

--- docker/hechms/runner.sh
+++ docker/hechms/runner.sh
@@ -1,1 +1,28 @@
 #!/usr/bin/env bash
+
+HOME_DIR="/mnt/disks/data/hechms/event"
+
+while getopts ":d:f:b:r:p:" option; do
+  case "${option}" in
+  d) EXEC_DATE=$OPTARG ;; # 2019-09-24 10:30:00
+  f) FORWARD=$OPTARG ;; # 3
+  b) BACKWARD=$OPTARG ;; # 2
+  r) INIT_RUN=$OPTARG ;; # 0 or 1
+  p) POP_METHOD=$OPTARG ;; # MME
+  esac
+done
+
+echo "EXEC_DATE : $EXEC_DATE"
+echo "FORWARD : $FORWARD"
+echo "BACKWARD : $BACKWARD"
+echo "INIT_RUN : $INIT_RUN"
+echo "POP_METHOD : $POP_METHOD"
+
+OUTPUT_DIR="${HOME_DIR}/$EXEC_DATE"
+mkdir -p ${OUTPUT_DIR}
+echo "OUTPUT_DIR : $OUTPUT_DIR"
+
+docker run -i --rm --privileged \
+    -v ${OUTPUT_DIR}:/home/curw/git/distributed_hechms/output  \
+    curw-hechms-centos7:hechms_4.2.1  /home/curw/hechms_run.sh -d ${EXEC_DATE} -f ${FORWARD} -b ${BACKWARD} -r ${INIT_RUN} -p ${POP_METHOD}
+
